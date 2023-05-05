50c7dd25e36785526d61c03834e81824532fe43a
santzero86 <132627153+santzero86@users.noreply.github.com>
2023-05-04 19:33:55 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+for (let index = 0; index < 99; index++) {
+    basic.showString("BANG!")
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.Ghost)
+}
+basic.forever(function () {
+	
+})
